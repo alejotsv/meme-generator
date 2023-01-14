f166375150f58099f7585725d1fc2a71722b7aec
Alejandro Salas <asalas.wd@outlook.com>
2023-01-13 21:19:28 -0500

    create meme img component

---
+++ src/components/MemeImg.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import shutup from '../assets/shutup.png';
+
+const MemeImg = () => {
+  return(
+    <img className='meme-img' src={shutup} />
+  )
+}
+
+export default MemeImg;
